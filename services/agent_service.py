6233a01156790f71826522279dbd3c1cea2e53a8
Will <yzj_000@163.com>
2026-06-29 23:42:22 +0800

    Switch to use Deep Agents for multi-agents

--- services/agent_service.py
+++ services/agent_service.py
@@ -3,20 +3,12 @@ import os
 from datetime import datetime
 
 import streamlit as st
-from langchain.tools import tool
+from deepagents import create_deep_agent, CompiledSubAgent
 from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
 
 from services.tools import web_search_tool, get_current_date
-
-
-@tool("research", description="Research a topic and return findings")
-def call_research_agent(query: str):
-    agent = research_agent()
-    result = agent.invoke({"messages": [HumanMessage(content=query)]})
-    return result["messages"][-1].content
 
 
 @st.cache_resource
@@ -31,19 +23,27 @@ def get_main_agent(model: str):
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
         model_provider="openai",
     )
-    return create_agent(
+    return create_deep_agent(
         model=llm,
-        tools=[get_current_date, call_research_agent],
+        tools=[get_current_date],
         system_prompt=f"You are a helpful assistant. Currently it's {datetime.now().year} this year, please offer your answers based on that.",
+        subagents=[research_agent()],
         checkpointer=get_checkpointer()
     )
 
 @functools.lru_cache(maxsize=1)
-def research_agent():
+def research_agent() -> CompiledSubAgent:
     llm = init_chat_model(
         model="qwen3.6-plus-2026-04-02",
         api_key=os.environ["DASHSCOPE_API_KEY"],
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
         model_provider="openai",
     )
-    return create_agent(model=llm, tools=[web_search_tool()])
+    return {
+        "name": "research-agent",
+        "description": "Used to research more in depth questions",
+        "runnable": create_agent(
+            model=llm,
+            tools=[web_search_tool()]
+        )
+    }
